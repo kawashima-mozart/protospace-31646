7e4e63458aea3e166cad4a27453406e339e8e37e
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-06 21:37:20 +0900

    編集のユーザー制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,8 +25,8 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    move_to_index
     @prototype = Prototype.find(params[:id])
+    move_to_index
   end
 
   def update
@@ -51,7 +51,7 @@ class PrototypesController < ApplicationController
   end
   
   def move_to_index
-    unless @user == current_user
+    unless current_user.id == @prototype.user_id
       redirect_to root_path
     end
   end
